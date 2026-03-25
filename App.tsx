9c916cad80091b1487211f8f42038541e0c6d7eb
MFisenko <maksim.fisenko@gmail.com>
2026-03-25 17:52:21 +0200

    Initial expo-auddio setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
+import { useAudioPlayer } from 'expo-audio';
+
+const audioSource = 'https://radio.dc.beltelecom.by/radiusfm/radiusfm.stream/playlist.m3u8';
 
 export default function App() {
+  const player = useAudioPlayer(audioSource);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Play Sound" onPress={() => player.play()} />
+      <Button
+        title="Replay Sound"
+        onPress={() => {
+          player.seekTo(0);
+          player.play();
+        }}
+      />
+      <Button
+        title="Pause Sound"
+        onPress={() => {
+          player.pause();
+        }}
+      />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
   },
 });
